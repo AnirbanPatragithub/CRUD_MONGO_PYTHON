3c5f821f9b3a2c966316a62317abeaa5046eb71e
Anirban Patra <96457415+AnirbanPatragithub@users.noreply.github.com>
2024-10-13 12:04:05 +0530

    Update user.py

--- schemas/user.py
+++ schemas/user.py
@@ -3,8 +3,8 @@ def userEntity(item) -> dict:
     print(item)
     return {
         'id':str(item['_id']),
-        'email':item['email'],
-        'location':item['location'],
+        'email':item.get('email','NA'),
+        'location':item.get('location','NA'),
         'clock_in':item.get('clock_in','NA')
         }
 
@@ -16,8 +16,8 @@ def itemEntity(item) -> dict:
     return {
         'id':str(item['_id']),
         'name':str(item['name']),
-        'email':item['email'],
-        'item_name':item['item_name'],
+        'email':item.get('email','NA'),
+        'item_name':item.get('item_name','NA'),
         'quantity':item.get('quantity','NA'),
         'expiry_date':item.get('expiry_date','NA'),
         'insert_date' : item.get('insert_date','NA')
